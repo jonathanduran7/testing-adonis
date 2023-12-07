ee1c8cec687760f9c34fda7b1b797c2a2af4b25e
jduran <jduran@gen-it.com.ar>
2023-12-07 13:50:35 -0300

    Fix test cases and add new test case for showing a note

--- tests/functional/note.spec.ts
+++ tests/functional/note.spec.ts
@@ -4,7 +4,7 @@ test.group('Notes', () => {
   test('display all notes', async ({ client }) => {
     const response = await client.get('/api/v1/notes')
 
-    response.assertStatus(200)    
+    response.assertStatus(200)
   })
 
   test('create a new note', async ({ client }) => {
@@ -26,5 +26,36 @@ test.group('Notes', () => {
       content: newNote.content,
       categoryId: newNote.categoryId
     })
+  })
+
+  test("don't show note if it's not exist", async ({ client }) => {
+    const response = await client.get('/api/v1/notes/1')
+
+    response.assertStatus(404)
+    response.assertBodyContains(
+      { message: 'No se encontró la nota' }
+    )
+  })
+
+  test('show a note', async ({ client }) => {
+    const response = await client.post('/api/v1/notes')
+      .accept('application/json')
+      .form({
+        title: 'New note',
+        content: 'This is a new note',
+        categoryId: '1'
+      })
+
+    const note = response.body()
+
+    const showResponse = await client.get(`/api/v1/notes/${note.id}`)
+
+    showResponse.assertStatus(200)
+    showResponse.assertBodyContains({
+      title: note.title,
+      content: note.content,
+      categoryId: Number(note.categoryId)
+    })
+   
   })
 })
