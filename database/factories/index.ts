a03650502f4c7397f815e9222917c4c9c0ef9948
jduran <jduran@gen-it.com.ar>
2023-12-19 15:28:38 -0300

    Add CategoryFactory to database/factories/index.ts

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,1 +1,11 @@
-// import Factory from '@ioc:Adonis/Lucid/Factory'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+import Category from 'App/Models/category.model'
+
+
+export const CategoryFactory = Factory.define(Category, ({faker}) => {
+    return {
+        name: faker.lorem.word(),
+        isActive: true
+    }
+}).build();
+
